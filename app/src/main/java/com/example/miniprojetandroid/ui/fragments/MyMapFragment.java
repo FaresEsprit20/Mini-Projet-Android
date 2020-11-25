e1892ce7555b136a19dd5c0aa08cf6222f95ff05
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2020-11-25 16:33:10 +0000

    sdsah

--- app/src/main/java/com/example/miniprojetandroid/ui/fragments/MyMapFragment.java
+++ app/src/main/java/com/example/miniprojetandroid/ui/fragments/MyMapFragment.java
@@ -4,6 +4,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -21,8 +22,34 @@ public class MyMapFragment extends SupportMapFragment implements OnMapReadyCallb
 
         // Set default position
         // Add a marker in Sydney and move the camera
-        LatLng shop = new LatLng(36.861001, 10.164021);  // 14.0583° N, 108.2772° E
+        LatLng shop = new LatLng( 36.861001, 10.164021);  // 14.0583° N, 108.2772° E
+        LatLng shop2 = new LatLng(36.807616, 10.137191);
+
+        LatLng community1 = new LatLng(36.58534834154057, 10.864349191947655);
+        LatLng community2 = new LatLng(36.460272, 10.758579);
+        LatLng community3 = new LatLng(35.792413, 10.592968);
+
+        LatLng cyclist1 = new LatLng(37.139099, 9.803709);
+        LatLng cyclist2 = new LatLng(36.838368, 10.096169);
+        LatLng cyclist3 = new LatLng(37.284892, 9.852577);
+
         this.googleMap.addMarker(new MarkerOptions().position(shop).title("Baskel Shop"));
+        this.googleMap.addMarker(new MarkerOptions().position(shop2).title("Baskel Shop 2"));
+        this.googleMap.addMarker(new MarkerOptions().position(community1).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title("Bike Community 1"));
+        this.googleMap.addMarker(new MarkerOptions().position(community2).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title("Bike Community 2"));
+        this.googleMap.addMarker(new MarkerOptions().position(community3).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title("Bike Community 3"));
+
+        this.googleMap.addMarker(new MarkerOptions().position(community2).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title("Bike Cyclist 1"));
+        this.googleMap.addMarker(new MarkerOptions().position(community3).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title("Bike Cyclist 2"));
+        this.googleMap.addMarker(new MarkerOptions().position(community3).icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title("Bike Cyclist 3"));
+
+
         this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(shop));
 
         this.googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -41,4 +68,3 @@ public class MyMapFragment extends SupportMapFragment implements OnMapReadyCallb
         });
     }
 }
-
